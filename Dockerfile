0b98a4185e4b51ef70e203d9a5888e08a30e68cd
Marais Rossouw <me@maraisr.com>
2019-04-10 11:04:26 +1000

    chore: Dockerfile and using actix

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM rust:latest as BUILDER
+
+RUN rustup target add x86_64-unknown-linux-musl
+RUN apt-get update
+RUN apt-get install -y \
+    build-essential \
+    musl-dev \
+    musl-tools
+
+WORKDIR /app
+
+ADD ["*.rs", "Cargo.*", "./"]
+
+RUN cargo build --target x86_64-unknown-linux-musl --release
+
+FROM scratch as RUNTIME
+
+EXPOSE 8080
+
+COPY --from=BUILDER /app/target/x86_64-unknown-linux-musl/release/app .
+
+CMD ["./app"]
--- index.rs
+++ index.rs
@@ -1,66 +1,44 @@
-extern crate http;
+extern crate actix_web;
 
 use std::collections::HashMap;
 
-use http::{Request, Response, StatusCode};
+use actix_web::{actix, App, http, HttpResponse, Path, server};
 
-fn handler(request: Request<()>) -> http::Result<Response<()>> {
-    let mut map = HashMap::new();
+static LISTEN_ON: &'static str = "0.0.0.0:8080";
+
+fn main() {
+    let sys = actix::System::new("app");
+
+    server::new(
+        || App::new()
+            .route("/{slug}", http::Method::GET, handler))
+        .bind(LISTEN_ON).unwrap()
+        .start();
+
+    println!("🚀 Started server on: {}", LISTEN_ON);
+
+    sys.run();
+}
+
+fn handler(slug: Path<String>) -> HttpResponse {
+    let mut map: HashMap<&'static str, &'static str> = HashMap::new();
     map.insert("twitter", "https://www.twitter.com");
 
     if cfg!(test) {
         map.insert("test", "test");
     }
 
-    let uri = &request.uri().path()[1..]; // the uri without the pre leading slash
+    if let Some(point_to) = map.get(&slug[..]) {
+        println!("⚡️ Found: {}, sending: {}", slug, *point_to);
 
-    if let Some(point_to) = map.get(uri) {
-
-        return Response::builder()
-            .status(StatusCode::FOUND)
-            .header("location", *point_to)
-            .body(())
+        return HttpResponse::Found()
+            .header(http::header::LOCATION, *point_to)
+            .header(http::header::CACHE_CONTROL, "public, s-maxage=43200, maxage=43200")
+            .finish();
+    } else {
+        println!("🤔 Asking for: {}", slug);
     }
 
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_should_find_something() {
-        let r = Request::builder()
-            .uri("/test")
-            .body(())
-            .unwrap();
-
-        assert_eq!(handler(r).unwrap().status(), StatusCode::FOUND);
-    }
-
-    #[test]
-    fn it_should_have_location_header() {
-        let r = Request::builder()
-            .uri("/test")
-            .body(())
-            .unwrap();
-
-        let handler_result = handler(r).unwrap();
-
-        assert_eq!(handler_result.status(), StatusCode::FOUND);
-        assert_eq!(handler_result.headers().get("location").is_some(), true);
-    }
-
-    #[test]
-    fn it_should_404() {
-        let r = Request::builder()
-            .uri("/test2")
-            .body(())
-            .unwrap();
-
-        assert_eq!(handler(r).unwrap().status(), StatusCode::NOT_FOUND);
-    }
+    HttpResponse::NotFound()
+        .finish()
 }
